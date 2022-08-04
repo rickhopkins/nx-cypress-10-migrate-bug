6a0ef904b79bf8ac7ac6b2d9823fad8d9793355f
Rick Hopkins <rick.hopkins@gmail.com>
2022-08-04 14:04:25 -0400

    initial commit

---
+++ nx-cypress-migrate-bug/apps/nx-cypress-migrate-bug/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-cypress-migrate-bug-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'nx-cypress-migrate-bug';
+}
